cc921cddfeec4a88006cb1e1aba9689c9464b3b0
siwoo <k74415834@gmail.com>
2025-01-14 03:20:39 +0900

    fix: jpa transaction manager 지정

--- src/main/java/com/project/yogerOrder/global/config/JpaConfig.java
+++ src/main/java/com/project/yogerOrder/global/config/JpaConfig.java
@@ -1,9 +1,19 @@
 package com.project.yogerOrder.global.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.JpaTransactionManager;
 
 @Configuration
 @EnableJpaAuditing // 테스트 시에 관련 bean을 로드하지 않기 때문에, 에러 발생 가능성이 있기에 메인에 적용하지 않음
 public class JpaConfig {
+
+    @Bean
+    @Primary
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
